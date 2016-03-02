4c4636a7b36fd693f2668960788e6a33d72f3879
Akira Fujiwara <fujiwara_akira@yahoo.co.jp>
2016-03-02 10:14:21 -0800

    2nd

--- twilio_respons.php
+++ twilio_respons.php
@@ -1,9 +1,9 @@
-&lt;?php
-if($_POST[&#39;Digits&#39;] == 1):
-  echo &quot;&lt;?xml version=\&quot;1.0\&quot; encoding=\&quot;UTF-8\&quot;?&gt;\n&quot;;
-  echo &#39;&lt;Response&gt;&#39;;
-  echo &#39;&lt;Say voice=&quot;woman&quot; language=&quot;ja-jp&quot;&gt;&#39;;
-   echo &#39;いちばんが入力されました&#39;;
-  echo &#39;&lt;/Say&gt;&#39;;
-  echo &#39;&lt;/Response&gt;&#39;;
+<?php
+if($_POST['Digits'] == 1):
+  echo "<?xml version=\"1.0\" encode=\"UTF-8\"?>\n";
+  echo '<Response>';
+  echo '<Say voice="woman" language="ja-jp">';
+   echo 'いちばんが入力されました';
+  echo '</Say>';
+  echo '</Responce>';
 endif;
